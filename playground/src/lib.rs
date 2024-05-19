6609b47bf6b5484ff0a82342f5d62385a3fde0f7
Mehmet Akif Yegin <94207328+makifyegin@users.noreply.github.com>
2024-05-19 17:51:57 +0000

    create a library and main file

---
+++ playground/src/lib.rs
@@ -0,0 +1,6 @@
+// Can you create a sum function
+// that takes two parameters and returns the sum of the two parameters?
+
+pub fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
+    //Can you use sum function inside the lib.rs
+    //to add two numbers?
+    let a = 10;
+    let b = 20;
+    let result = playground::sum(a, b);
+    println!("The sum of {} and {} is {}", a, b, result);
+
 }
